f1dde939564d5132fda95312a9ca0c431bd76e7c
Thomas Hareau <thomashareau@free.fr>
2018-04-20 17:05:39 +0200

    moving classes into several files, and making non-private useful functions

---
+++ src/u2flib_server/Registration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace u2flib_server;
+
+/**
+ * Class returned for successful registrations
+ *
+ * @package u2flib_server
+ */
+class Registration
+{
+    /** @var string The key handle of the registered authenticator */
+    public $keyHandle = '';
+
+    /** @var string The public key of the registered authenticator */
+    public $publicKey = '';
+
+    /** @var string The attestation certificate of the registered authenticator */
+    public $certificate = '';
+
+    /** @var int The counter associated with this registration */
+    public $counter = -1;
+}
