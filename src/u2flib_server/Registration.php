71ad60cd25c2636866fc87c4d4519373cec2de0c
Thomas Hareau <thomashareau@free.fr>
2018-04-23 14:08:46 +0200

    using autoloader

--- src/u2flib_server/Registration.php
+++ src/u2flib_server/Registration.php
@@ -3,21 +3,21 @@
 namespace u2flib_server;
 
 /**
- * Class returned for successful registrations
+ * Class returned for successful registrations.
  *
  * @package u2flib_server
  */
 class Registration
 {
-    /** @var string The key handle of the registered authenticator */
+    /** @var string The key handle of the registered authenticator. */
     public $keyHandle = '';
 
-    /** @var string The public key of the registered authenticator */
+    /** @var string The public key of the registered authenticator. */
     public $publicKey = '';
 
-    /** @var string The attestation certificate of the registered authenticator */
+    /** @var string The attestation certificate of the registered authenticator. */
     public $certificate = '';
 
-    /** @var int The counter associated with this registration */
+    /** @var int The counter associated with this registration. */
     public $counter = -1;
 }
--- src/u2flib_server/SignRequest.php
+++ src/u2flib_server/SignRequest.php
@@ -3,16 +3,16 @@
 namespace u2flib_server;
 
 /**
- * Class for building up an authentication request
+ * Class for building up an authentication request.
  *
  * @package u2flib_server
  */
 class SignRequest
 {
-    /** @var string Protocol version */
+    /** @var string Protocol version. */
     public $version = U2F_VERSION;
 
-    /** @var string Authentication challenge */
+    /** @var string Authentication challenge. */
     public $challenge = '';
 
     /** @var string Key handle of a registered authenticator */
